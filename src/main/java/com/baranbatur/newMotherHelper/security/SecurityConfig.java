078047134619935616572d1c046d9d9affb605f3
baranbaturr01 <baturbarann@gmail.com>
2024-08-26 16:40:38 +0300

    token time was updated

--- src/main/java/com/baranbatur/newMotherHelper/security/SecurityConfig.java
+++ src/main/java/com/baranbatur/newMotherHelper/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8082"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8081"));
         configuration.setAllowedMethods(List.of("GET", "POST"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
