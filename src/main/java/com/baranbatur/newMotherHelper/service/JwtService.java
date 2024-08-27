e5be4a195ee296d1378a1bd550ddb095948e54f8
baranbaturr01 <baturbarann@gmail.com>
2024-08-27 15:48:08 +0300

    jenkins was integreted

--- src/main/java/com/baranbatur/newMotherHelper/service/JwtService.java
+++ src/main/java/com/baranbatur/newMotherHelper/service/JwtService.java
@@ -46,7 +46,7 @@ public class JwtService {
                 .add(extraClaims)
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .expiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 365))
                 .and()
                 .signWith(getSignInKey(), Jwts.SIG.HS256)
                 .compact();
